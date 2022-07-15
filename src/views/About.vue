8e5ee0b7b17e88185da62d48a28e791fb044d59e
AndyHsu0506 <108677037+AndyHsu0506@users.noreply.github.com>
2022-07-15 18:28:27 +0800

    #網站連結# 字體 打字速度 字體大小

--- src/views/About.vue
+++ src/views/About.vue
@@ -19,7 +19,16 @@ And I'm 16 years old in 2022, also a student, studing at HYSH.
 
 This summer I went to NTNU CSIE Camp to learn how to code.
 Here's what I learned :
-" />
+" :speed="45" />
+<a
+    v-if="step >=4"
+    class="mt-8 flex items-center text-lg text-gray-500 transition-all hover:text-gray-700 absolute -inset-y-15 left-15"
+    href="http://localhost:3000/projects"
+    target="_blank"
+    >
+    <MdiGithub class="mr-1 inline-block" />
+    <Typing v-if="step >= 4" @done="step++" text="My Projets" class="italic text-2xl" :speed="80" />
+</a>
 <img src="https://p3-tt.byteimg.com/origin/pgc-image/768380d0c8c64873a1264091fb6271a9?from=pc.jpg" class="object-cover h-48 w-48 absolute bottom-8 left-0"/>
 <Typing v-if="step >= 1" @done="step++" text= "Happy Dog~ " class="absolute bottom-0 left-12"/>
 <img src="https://img.ttshow.tw/images/media/uploads/2019/10/04/65a92dc9gy3g7awfnvfvsj20mi0u0ds5.jpg" class="object-cover h-96 w-48 absolute inset-y-12 right-20 "/>
